afff96f4078c6c146337e2458664456e2901ae10
Alexandru Miron <mironalex96@gmail.com>
2018-03-27 12:28:56 +0300

    Finish sparse matrix addition

--- hw3/main.py
+++ hw3/main.py
@@ -1,9 +1,12 @@
 import hw3.sparse as sparse
+import time
 
 a_matrix = sparse.SparseList()
 b_matrix = sparse.SparseList()
+aplusb_matrix = sparse.SparseList()
 ax = []
 bx = []
+abx = []
 
 
 def solve_aplusb():
@@ -27,6 +30,20 @@ def solve_aplusb():
             values = str.split(line, ",")
             b_matrix.insert(float(values[0]), int(values[1]), int(values[2]))
 
+    with open('aplusb.txt') as f:
+        n = int(f.readline())
+        f.readline()
+        for i in range(0, n):
+            abx.append(float(f.readline()))
+        f.readline()
+        for line in f:
+            values = str.split(line, ",")
+            aplusb_matrix.insert(float(values[0]), int(values[1]), int(values[2]))
 
+    start = time.time()
+    result_aplusb = a_matrix + b_matrix
+    end = time.time()
+    print(end - start)
+    print(result_aplusb == aplusb_matrix)
 
 solve_aplusb()
--- hw3/sparse.py
+++ hw3/sparse.py
@@ -1,4 +1,7 @@
 """The required implementation of sparse matrix"""
+
+epsilon = 10e-10
+
 class SparseList:
     def __init__(self, rows=0, columns=0, values=None):
         self.rows = rows
@@ -7,11 +10,39 @@ class SparseList:
         if values is None:
             self.values = [[]]
 
+    def __eq__(self, other):
+        if self.rows != other.rows or self.columns != other.columns:
+            return False
+        for line in range(0, self.rows):
+            if len(self.values[line]) != len(other.values[line]):
+                return False
+            self.values[line] = sorted(self.values[line], key=lambda entry: entry[1])
+            other.values[line] = sorted(other.values[line], key=lambda entry: entry[1])
+            for column in range(0, len(self.values[line])):
+                if self.values[line][column][1] != other.values[line][column][1]:
+                    return False
+                if abs(self.values[line][column][0] - other.values[line][column][0]) > epsilon:
+                    return False
+        return True
+
     def __add__(self, other):
         if self.rows != other.rows or self.columns != other.columns:
             return None
         else:
-            pass
+            values = self.values
+            for i in range(0, self.rows):
+                if other.values[i]:
+                    for j in range(0, len(other.values[i])):
+                        found = False
+                        for k in range(0, len(values[i])):
+                            if values[i][k][1] == other.values[i][j][1]:
+                                values[i][k] = (values[i][k][0] + other.values[i][j][0], values[i][k][1])
+                                found = True
+                                break
+                        if not found:
+                            values[i].insert(0, other.values[i][j])
+
+            return SparseList(self.rows, self.columns, values)
 
     def insert(self, value, line, column):
         if line > self.rows:
@@ -20,18 +51,19 @@ class SparseList:
             self.rows = line
         if column > self.columns:
             self.columns = column
-        if line == column:
-            self.values[line].append((value, column))
-        else:
-            self.values[line].insert(0, (value, column))
+        for i in range(0, len(self.values[line])):
+            if self.values[line][i][1] == column:
+                self.values[line][i][0] += value
+                return
+        self.values[line].append((value, column))
 
 
 class SparseCSR:
     """An implementation of Compressed sparse row sparse matrix"""
+
     def __init__(self):
         self.rows = 0
         self.columns = 0
         self.values = []
         self.column_idx = []
         self.row_start = []
-
