3961a15f21ff09b4903db3cc676b7fe5b0885f27
Alexandru Miron <mironalex96@gmail.com>
2018-05-15 13:11:50 +0300

    finished first 2 tasks?

--- hw6/main.py
+++ hw6/main.py
@@ -1,7 +1,9 @@
 from hw3 import sparse
 import random
+import numpy as np
 
-epsilon = 10e-10
+epsilon = 10e-15
+max_iterations = 10e7
 
 
 def generate_symmetric_rare_matrix(n):
@@ -13,6 +15,15 @@ def generate_symmetric_rare_matrix(n):
         column = random.randint(0, n)
         result.insert(value, line, column)
         result.insert(value, column, line)
+    return result
+
+
+def generate_random_initial_vector(n):
+    result = []
+    for i in range(0, n):
+        value = (random.random() + epsilon) * 10
+        result.append(value)
+    result = np.multiply(1 / np.linalg.norm(result), result)
     return result
 
 
@@ -28,5 +39,31 @@ def read_sparse_matrix(filename):
     return result
 
 
+def is_symmetric(matrix: sparse.SparseList):
+    transposed = matrix.get_transposed()
+    if matrix == transposed:
+        return True
+    return False
+
+
+def power_method(matrix: sparse.SparseList):
+    n = matrix.rows+1
+    v = generate_random_initial_vector(n).tolist()
+    w = v * matrix
+    lambda_current = np.dot(w, v)
+    iteration = 0
+    while iteration < max_iterations and np.linalg.norm(w - lambda_current) < n*epsilon:
+        v = np.multiply(1 / np.linalg.norm(w), w)
+        w = v * matrix
+        lambda_current = np.dot(w, v)
+        iteration += 1
+    return lambda_current
+
+
 if __name__ == '__main__':
-    pass
+    random_rare_matrix = generate_symmetric_rare_matrix(501)
+    print(power_method(random_rare_matrix))
+
+    input_matrix = read_sparse_matrix("m_rar_sim_2018.txt")
+    print(is_symmetric(input_matrix))
+    print(power_method(input_matrix))
